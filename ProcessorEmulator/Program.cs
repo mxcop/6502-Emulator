0ea510bfdf59de29ae44577339301cb0d536d086
MaxCoppen <44031065+MaxCoppen@users.noreply.github.com>
2021-03-13 21:04:47 +0100

    some fun code

--- ProcessorEmulator/Program.cs
+++ ProcessorEmulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ascii.Engine;
 using Ascii.Rendering;
+using Ascii.Types;
 using System;
 
 namespace ProcessorEmulator
@@ -17,7 +18,38 @@ namespace ProcessorEmulator
             AsciiEngine.OnKeyPressed += gameManager.KeyPressed;
             AsciiEngine.OnRender += gameManager.Render;
 
+            //AsciiEngine.OnStart += () =>
+            //{
+            //    Random rand = new Random();
+
+            //    AsciiRenderer.SetBackground(AsciiColor.FromRGB(24, 20, 37));
+
+            //    for (int y = 0; y < 50; y++)
+            //    {
+            //        for (int x = 0; x < 80; x++)
+            //        {
+            //            int r = rand.Next(0, 16);
+            //            char c = ' ';
+
+            //            c = r switch
+            //            {
+            //                10 => 'A',
+            //                11 => 'B',
+            //                12 => 'C',
+            //                13 => 'D',
+            //                14 => 'E',
+            //                15 => 'F',
+            //                _ => ("" + r)[0],
+            //            };
+
+            //            int r2 = rand.Next(-15, 5);
+            //            AsciiRenderer.SetChar(x, y, c, AsciiColor.FromRGB((byte)(38 + r2), (byte)(43 + r2), (byte)(68 + r2)));
+            //        }
+            //    }
+            //};
+
             AsciiEngine.Initialize(60);
+            
         }
     }
 }
